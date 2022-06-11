16e782a80ae9c80ba23dafb3aa8f634f97846987
WilsonWang <wi0143@gmail.com>
2022-06-11 15:18:28 +0800

    2

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  try{
     res.render('Turnkey', { title: 'Express' });
-  }catch(err){
-    console.log(err);
-  }
-  
 });
 
 module.exports = router;
